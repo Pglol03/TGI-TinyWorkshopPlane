371b57876a8d0a43fd6861ae9ae653d906899672
Pradyun Gayakwad <pradyungayakwad@gmail.com>
2023-02-08 21:53:20 +0530

    Wifi creds added

--- Workshop Plane.ino
+++ Workshop Plane.ino
@@ -22,8 +22,8 @@ unsigned long premillis_rssi = 0;
 unsigned long premillis_rx   = 0;
 
 int status = WL_IDLE_STATUS;
-char ssid[] = "wifiplane";   //  your network SSID (name)
-char pass[] = "wifiplane1234";    // your network password (use for WPA, or use as key for WEP)
+char ssid[] = "ONEPLUS_Pradyun";   //  your network SSID (name)
+char pass[] = "pglol@lol";    // your network password (use for WPA, or use as key for WEP)
 int keyIndex = 0;            // your network key Index number (needed only for WEP)
 IPAddress remotIp;
 unsigned int localPort = 6000;      // local port to listen on
